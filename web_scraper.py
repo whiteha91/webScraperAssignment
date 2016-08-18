8ba11e1ede90a5285e7364ef7a2cd37ca84cbbeb
whiteha91 <whiteha91@gmail.com>
2016-08-18 18:12:37 +1200

    First Commit
    Small Updates

--- web_scraper.py
+++ web_scraper.py
@@ -2,52 +2,54 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-url = "http://pokemondb.net/pokedex/"
 
+class WebScraper:
+    url = "http://pokemondb.net/pokedex/"
+    gen = 1
+    type = "Fire"
 
-def list_gen(url):
-    r = requests.get(url + "national").text
+    def __init__(self, url="http://pokemondb.net/pokedex/", gen=1, p_type="Fire"):
+        self.url = url
+        self.gen = gen
+        self.type = p_type
 
-    soup = BeautifulSoup(r, "html.parser")
-    container = soup.find('div', attrs={'class': 'infocard-tall-list'})
-    cards = container.find_all('span')
-    pokemon_info = []
-    for card in cards:
-        gen = card.find('a')
-        type = card.find('small', attrs={'class': 'aside'}).text
-        # print(type)
-        # print(gen)
-        if re.search('G1', gen['data-sprite']) and re.search('Fire', type):
-            pokemon_info.append(card.find('a', attrs={'class': 'ent-name'}).text)
-            # print(card.find('a', attrs={'class': 'ent-name'}))
-        # pokemon_info = card.find_all('a', attrs={'class': 'ent-name'})
-        # for pokemon in pokemon_info:
-        #     pokemon_list.append(pokemon_info)
-    return pokemon_info
+    def list_gen(self,):
+        r = requests.get(self.url + "national").text
+        soup = BeautifulSoup(r, "html.parser")
+        container = soup.find('div', attrs={'class': 'infocard-tall-list'})
+        cards = container.find_all('span')
+        pokemon_list = []
+        for card in cards:
+            gen = card.find('a')
+            type_info = card.find('small', attrs={'class': 'aside'}).text
+            # print(type_info)
+            # print(gen)
+            if re.search('G' + str(gen), gen['data-sprite']) and re.search(self.type, type_info):
+                pokemon_list.append(card.find('a', attrs={'class': 'ent-name'}).text)
+                # print(card.find('a', attrs={'class': 'ent-name'}))
+            # pokemon_list = card.find_all('a', attrs={'class': 'ent-name'})
+            # for pokemon in pokemon_list:
+            #     pokemon_list.append(pokemon_list)
+        return pokemon_list
 
-
-def info_grab(pokemon):
-    r = requests.get(url + pokemon).text
-    soup = BeautifulSoup(r, "html.parser")
-    desc = soup.find('div', attrs={'class': 'col desk-span-8 lap-span-6'})
-    table = soup.find('div', attrs={'class': 'tabset-basics'})
-    img = table.find('img')
-    img = img['src']
-    print(img)
-    container = table.find('div', attrs={'class': 'col desk-span-4 lap-span-6'})
-    basic_data = container.find('table', attrs={'class': 'vitals-table'})
-    rows = basic_data.find_all('tr')
-    for row in rows:
-        if re.search('National', row.text):
-            print(row.find('td').text)
-        elif re.search('Type', row.text):
-            print(row.find('td').text[1:])
-        elif re.search('Height', row.text):
-            print(row.find('td').text[row.find('td').text.index('(')+1:row.find('td').text.index(')')-1])
-        elif re.search('Weight', row.text):
-            print(row.find('td').text[row.find('td').text.index('(')+1:row.find('td').text.index(')')-3])
-
-for pokemon in list_gen(url):
-    print(pokemon)
-    info_grab(pokemon)
-
+    def info_grab(self, pokemon,):
+        r = requests.get(self.url + pokemon).text
+        soup = BeautifulSoup(r, "html.parser")
+        desc = soup.find('div', attrs={'class': 'col desk-span-8 lap-span-6'})
+        table = soup.find('div', attrs={'class': 'tabset-basics'})
+        img = table.find('img')
+        img = img['src']
+        print(img)
+        container = table.find('div', attrs={'class': 'col desk-span-4 lap-span-6'})
+        basic_data = container.find('table', attrs={'class': 'vitals-table'})
+        rows = basic_data.find_all('tr')
+        for row in rows:
+            if re.search('National', row.text):
+                number = int(row.find('td').text)
+            elif re.search('Type', row.text):
+                pokemon_type = row.find('td').text[1:]
+            elif re.search('Height', row.text):
+                height = float(row.find('td').text[row.find('td').text.index('(')+1:row.find('td').text.index(')')-1])
+            elif re.search('Weight', row.text):
+                weight = float(row.find('td').text[row.find('td').text.index('(')+1:row.find('td').text.index(')')-3])
+        return {"name": pokemon, "image": img, "number": number, "type": pokemon_type, "height": height, "weight": weight}
