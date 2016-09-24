7b62bd7499f57dd49b9cbe620bbbb1e16ab4050d
whiteha91 <whiteha91@gmail.com>
2016-09-24 16:18:35 +1200

    Name Change stage 2
    The second variable name was changed to be generic too

--- StatisticCalculator.py
+++ StatisticCalculator.py
@@ -7,21 +7,21 @@ class StatisticCalculator:
 
     def get_min(self, pokedex, measurable):
         min_value = 1000000000
-        lightest_pokemon = ""
+        pokemon = ""
         for name in pokedex:
             if pokedex[name].get_weight() < min_value:
                 min_value = pokedex[name].get_weight()
-                lightest_pokemon = name
-        return lightest_pokemon
+                pokemon = name
+        return pokemon
 
     def get_max(self, pokedex, measurable):
         max_value = -1
-        heaviest_pokemon = ""
+        pokemon = ""
         for name in pokedex:
             if pokedex[name].get_weight() > max_value:
                 max_value = pokedex[name].get_weight()
-                heaviest_pokemon = name
-        return heaviest_pokemon
+                pokemon = name
+        return pokemon
 
     def get_avg(self, pokedex, measurable):
         num_of_pokemon = len(pokedex)
