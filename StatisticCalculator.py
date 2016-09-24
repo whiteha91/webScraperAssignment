77925d4b2e6e557ce668dff837dd9d28164256d3
whiteha91 <whiteha91@gmail.com>
2016-09-24 16:11:43 +1200

    Variable renaming
    Varible names changed to their generic version

--- StatisticCalculator.py
+++ StatisticCalculator.py
@@ -6,29 +6,29 @@
 class StatisticCalculator:
 
     def get_min(self, pokedex, measurable):
-        min_weight = 1000000000
+        min_value = 1000000000
         lightest_pokemon = ""
         for name in pokedex:
-            if pokedex[name].get_weight() < min_weight:
-                min_weight = pokedex[name].get_weight()
+            if pokedex[name].get_weight() < min_value:
+                min_value = pokedex[name].get_weight()
                 lightest_pokemon = name
         return lightest_pokemon
 
     def get_max(self, pokedex, measurable):
-        max_weight = -1
+        max_value = -1
         heaviest_pokemon = ""
         for name in pokedex:
-            if pokedex[name].get_weight() > max_weight:
-                max_weight = pokedex[name].get_weight()
+            if pokedex[name].get_weight() > max_value:
+                max_value = pokedex[name].get_weight()
                 heaviest_pokemon = name
         return heaviest_pokemon
 
     def get_avg(self, pokedex, measurable):
         num_of_pokemon = len(pokedex)
-        sum_of_weights = 0
+        sum_of_values = 0
         for name in pokedex:
-            sum_of_weights += pokedex[name].get_weight()
-        avg = sum_of_weights / num_of_pokemon
+            sum_of_values += pokedex[name].get_weight()
+        avg = sum_of_values / num_of_pokemon
         return avg
 
     def get_min_height(self, pokedex):
