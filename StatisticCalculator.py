bf6d177a8cbda434a26c9bee60c79fc15ee52297
whiteha91 <whiteha91@gmail.com>
2016-09-24 16:49:15 +1200

    Duplication Removal
    The old duplicate code (now unused) was removed

--- StatisticCalculator.py
+++ StatisticCalculator.py
@@ -42,29 +42,3 @@ class StatisticCalculator:
             sum_of_values += value
         avg = sum_of_values / num_of_pokemon
         return avg
-
-    def get_min_height(self, pokedex):
-        min_height = 1000000000
-        shortest_pokemon = ""
-        for name in pokedex:
-            if pokedex[name].get_height() < min_height:
-                min_height = pokedex[name].get_height()
-                shortest_pokemon = name
-        return shortest_pokemon
-
-    def get_max_height(self, pokedex):
-        max_height = -1
-        tallest_pokemon = ""
-        for name in pokedex:
-            if pokedex[name].get_height() > max_height:
-                max_height = pokedex[name].get_height()
-                tallest_pokemon = name
-        return tallest_pokemon
-
-    def get_avg_height(self, pokedex):
-        num_of_pokemon = len(pokedex)
-        sum_of_heights = 0
-        for name in pokedex:
-            sum_of_heights += pokedex[name].get_height()
-        avg = sum_of_heights / num_of_pokemon
-        return avg
