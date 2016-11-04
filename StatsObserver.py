fa9b47b6bd715b5600360cca4f078055acd55a4e
whiteha91 <whiteha91@gmail.com>
2016-11-04 21:48:15 +1300

    pep8 fixes

--- StatsObserver.py
+++ StatsObserver.py
@@ -22,20 +22,20 @@ class StatsObserver(Observer.Observer):
             print("Weight: " + str(species.get_weight()) + "kg")
         elif stat[0] != "":
             if stat[2] == "min_weight":
-                print("the lightest pokemon you have got data on is " + stat[0] +
-                      " at only " + str(stat[1]) + "kg")
+                print("the lightest pokemon you have got data on is " + stat[0]
+                      + " at only " + str(stat[1]) + "kg")
             elif stat[2] == "max_weight":
-                print("the heaviest pokemon you have got data on is " + stat[0] +
-                      " at a whooping " + str(stat[1]) + "kg")
+                print("the heaviest pokemon you have got data on is " + stat[0]
+                      + " at a whooping " + str(stat[1]) + "kg")
             elif stat[2] == "avg_weight":
-                print("the average weight of pokemon you have got data on is " +
-                      str(stat[1]) + "kg")
+                print("the average weight of pokemon you have got data on is "
+                      + str(stat[1]) + "kg")
             elif stat[2] == "min_height":
-                print("the shortest pokemon you have got data on is " + stat[0] +
-                      " at only " + str(stat[1]) + "m")
+                print("the shortest pokemon you have got data on is " + stat[0]
+                      + " at only " + str(stat[1]) + "m")
             elif stat[2] == "max_height":
-                print("the tallest pokemon you have got data on is " + stat[0] +
-                      " at a whooping " + str(stat[1]) + "m")
+                print("the tallest pokemon you have got data on is " + stat[0]
+                      + " at a whooping " + str(stat[1]) + "m")
             elif stat[2] == "avg_height":
-                print("the average height of pokemon you have got data on is " +
-                      str(stat[1]) + "m")
+                print("the average height of pokemon you have got data on is "
+                      + str(stat[1]) + "m")
